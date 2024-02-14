a0cf97806d06ed21141d49ab0f08c2b8876b2a52
Bibek <study.bibekshrestha@gmail.com>
2024-02-14 11:57:21 +0200

    minor changes

--- app/src/main/java/com/example/sbma_project/MainActivity.kt
+++ app/src/main/java/com/example/sbma_project/MainActivity.kt
@@ -1,33 +1,40 @@
 package com.example.sbma_project
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.*
+import com.example.sbma_project.pemissionManager.PermissionRequestDialog
+import com.example.sbma_project.pemissionManager.PermissionUtils
+import com.example.sbma_project.pemissionManager.PermissionUtils.hasAllPermission
+import com.example.sbma_project.pemissionManager.PermissionUtils.hasLocationPermission
+import com.example.sbma_project.pemissionManager.PermissionUtils.openAppSetting
 import com.example.sbma_project.ui.theme.SBMAProjectTheme
 
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.NavHost
-
-
-
+@RequiresApi(Build.VERSION_CODES.S)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SBMAProjectTheme {
-                // A surface container using the 'background' color from the theme
+                // request permission
+                PermissionRequester()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -38,7 +45,50 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    private fun PermissionRequester() {
+        var showPermissionDeclinedRationale by rememberSaveable { mutableStateOf(false) }
+        var showRationale by rememberSaveable { mutableStateOf(false) }
+        val permissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = {
+                it.forEach { (permission, isGranted) ->
+                    if (!isGranted && PermissionUtils.locationPermissions.contains(permission)) {
+                        showPermissionDeclinedRationale = true
+                    }
+                }
+            }
+        )
+        if (showPermissionDeclinedRationale)
+            PermissionRequestDialog(
+                onDismissClick = {
+                    if (!hasLocationPermission())
+                        finish()
+                    else showPermissionDeclinedRationale = false
+                },
+                onOkClick = { openAppSetting() }
+            )
+        if (showRationale)
+            PermissionRequestDialog(
+                onDismissClick = ::finish,
+                onOkClick = {
+                    showRationale = false
+                    permissionLauncher.launch(PermissionUtils.allPermissions)
+                }
+            )
+        LaunchedEffect(key1 = Unit) {
+            when {
+                hasAllPermission() -> return@LaunchedEffect
+                PermissionUtils.locationPermissions.any { shouldShowRequestPermissionRationale(it) } -> showRationale =
+                    true
+                else -> permissionLauncher.launch(PermissionUtils.allPermissions)
+            }
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
