2d12031df20108bb1cb1bb1249e4598fb91bc0b2
Bibek <study.bibekshrestha@gmail.com>
2024-02-14 23:38:56 +0200

    inor changes

--- app/src/main/java/com/example/sbma_project/views/MapView.kt
+++ app/src/main/java/com/example/sbma_project/views/MapView.kt
@@ -1,17 +1,64 @@
 package com.example.sbma_project.views
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
-fun MapView(){
-    Box(modifier = Modifier.fillMaxSize()){
-        GoogleMap {
+fun MapView(activity: ComponentActivity) {
+    val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(activity)
 
-        }
+    var currentLocation by remember {
+        mutableStateOf(LatLng(0.toDouble(), 0.toDouble()))
     }
 
+    val mapProperties = MapProperties(
+        isMyLocationEnabled = true
+    )
+
+    val cameraPositionState = rememberCameraPositionState()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            properties = mapProperties,
+            cameraPositionState = cameraPositionState
+        )
+    }
+
+    // Request location permission
+    LaunchedEffect(true) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        } else {
+            val location = fusedLocationClient.lastLocation.isSuccessful
+            cameraPositionState.cameraPosition = location.latitude to location.longitude
+        }
+    }
 }
