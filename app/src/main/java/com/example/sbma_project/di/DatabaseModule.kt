1c7854084563634a1d9627f0a48d61bcd1e368da
Bibek <study.bibekshrestha@gmail.com>
2024-02-24 22:35:28 +0200

    started timer logic to DB

---
+++ app/src/main/java/com/example/sbma_project/di/DatabaseModule.kt
@@ -0,0 +1,17 @@
+package com.example.sbma_project.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+
+
+}
