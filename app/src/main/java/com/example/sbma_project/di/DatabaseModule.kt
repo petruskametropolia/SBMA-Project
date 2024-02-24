f565c7f6a8ed1de6075565e947358c65164c9ca8
Bibek <study.bibekshrestha@gmail.com>
2024-02-24 22:37:44 +0200

    timer logic to DB

--- app/src/main/java/com/example/sbma_project/di/DatabaseModule.kt
+++ app/src/main/java/com/example/sbma_project/di/DatabaseModule.kt
@@ -1,17 +1,29 @@
 package com.example.sbma_project.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.sbma_project.database.RunDatabase
+import com.example.sbma_project.database.TimerDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-
-    @Singleton
     @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context) : RunDatabase {
+        return Room.databaseBuilder(context, RunDatabase::class.java, "run_database")
+            .build()
+    }
+
+    @Provides
+    fun provideTimerDao(runDatabase: RunDatabase) : TimerDao {
+        return runDatabase.timerDao()
+    }
 
 
 }
--- app/src/main/java/com/example/sbma_project/repository/TimerRepository.kt
+++ app/src/main/java/com/example/sbma_project/repository/TimerRepository.kt
@@ -2,7 +2,15 @@ package com.example.sbma_project.repository
 
 import androidx.room.Dao
 import com.example.sbma_project.database.TimerDao
+import dagger.hilt.InstallIn
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+
 class TimerRepository @Inject constructor(timerDao: TimerDao) {
+}
+
+@HiltViewModel
+class TimerViewModel @Inject constructor(timerRepository: TimerRepository){
+
 }
