40e96ebea9942f0e8ed71e529153a94f3b312431
Bibek <study.bibekshrestha@gmail.com>
2024-02-24 22:42:41 +0200

    timer logic to DB

--- app/src/main/java/com/example/sbma_project/repository/TimerRepository.kt
+++ app/src/main/java/com/example/sbma_project/repository/TimerRepository.kt
@@ -1,16 +1,28 @@
 package com.example.sbma_project.repository
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import com.example.sbma_project.database.Timer
 import com.example.sbma_project.database.TimerDao
 import dagger.hilt.InstallIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
-class TimerRepository @Inject constructor(timerDao: TimerDao) {
+class TimerRepository @Inject constructor(private val timerDao: TimerDao) {
+
+    suspend fun insertTimer(timer: Timer) {
+        timerDao.insertTimer(timer)
+    }
+
+    fun getActiveTimerFlow(): LiveData<List<Timer>> {
+        return timerDao.getAllTimers()
+    }
 }
 
 @HiltViewModel
-class TimerViewModel @Inject constructor(timerRepository: TimerRepository){
+class TimerViewModel @Inject constructor(private val timerRepository: TimerRepository){
 
 }
+
+
