ab7005bf2cd5fc184908e4a88a2ec7e814239f04
Bibek <study.bibekshrestha@gmail.com>
2024-02-26 00:11:31 +0200

    added card to history

--- app/src/main/java/com/example/sbma_project/views/History.kt
+++ app/src/main/java/com/example/sbma_project/views/History.kt
@@ -1,14 +1,22 @@
 package com.example.sbma_project.views
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import com.example.sbma_project.database.Timer
 import com.example.sbma_project.repository.TimerViewModel
@@ -32,5 +40,19 @@ fun History(timerViewModel: TimerViewModel) {
 
 @Composable
 fun TimerItem(timer: Timer) {
-    Text(text = "Timer ID: ${timer.id}, Duration: ${timer.durationInMillis}s, path array size : ${timer.routePath?.size}")
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "ID: ${timer.id}")
+            Text(text = "Duration: ${timer.durationInMillis}s")
+            Text(text = "route LatLng size: ${timer.routePath?.size}")
+        }
+    }
 }
